ce4be9879c63546ed325c9dadb1e3400d4632c57
Fatih BÜLBÜL <maccfatih@gmail.com>
2016-06-16 23:50:59 +0300

    demo

--- toastr.Net.Demo/Controllers/HomeController.cs
+++ toastr.Net.Demo/Controllers/HomeController.cs
@@ -10,7 +10,8 @@ namespace toastr.Net.Demo.Controllers
         {
             ViewBag.Message =
                 Notification.Show("Merhaba dünya", onclick: "hello") +
-                Notification.Show("Elveda dünya", position: Position.BottomCenter, type: ToastType.Error, timeOut: 7000);
+                Notification.Show("Elveda dünya", position: Position.BottomCenter, type: ToastType.Error, timeOut: 7000) +
+                Notification.Show("System of down", "Error", type: ToastType.Error);
             return View();
         }
     }
